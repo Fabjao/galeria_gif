804a274df30a48feb07e519b45fdb3480a438b62
Fabio Gonçalves <fabio.g@inttecnologia.com.br>
2020-01-10 18:24:53 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:buscar_gifs/ui/home_page.dart';
+
+void main() {
+  runApp(
+      MaterialApp(theme: ThemeData(hintColor: Colors.white), home: HomePage()));
+}
